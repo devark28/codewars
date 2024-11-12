c5d12527cb98239b216e9d18c67a904cdbfa0506
devark28 <e.shimwa1@alustudent.com>
2024-11-12 11:12:19 +0200

    Trial

--- 6 kyu/Are they the same.js
+++ 6 kyu/Are they the same.js
@@ -2,10 +2,11 @@ function comp(array1, array2){
     let result = false;
 
     for(let e1 of array1) {
+        console.log(e1);
         const sq = e1 ** 2;
         let sq_index = -1;
         for(let e2_i in array2){
-            console.log(sq, array2[e2_i])
+            console.log(sq, array2[e2_i], sq === array2[e2_i]);
             if(sq === array2[e2_i]){
                 sq_index = e2_i;
                 break;
