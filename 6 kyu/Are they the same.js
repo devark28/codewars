91d3d5207c675d34b940e7f3ea84c7fe42f7bf95
devark28 <e.shimwa1@alustudent.com>
2024-11-12 11:13:23 +0200

    Trial

--- 6 kyu/Are they the same.js
+++ 6 kyu/Are they the same.js
@@ -1,13 +1,15 @@
 function comp(array1, array2){
     let result = [];
+    const arr1 = array1.filter((e, i) => array1.indexOf(e) !== i)
+    const arr2 = array2.filter((e, i) => array2.indexOf(e) !== i)
 
-    for(let e1 of array1) {
+    for(let e1 of arr1) {
         console.log(e1);
         const sq = e1 ** 2;
         let sq_index = -1;
-        for(let e2_i in array2){
-            if(sq === array2[e2_i]){
-                console.log(sq, array2[e2_i], sq === array2[e2_i]);
+        for(let e2_i in arr2){
+            if(sq === arr2[e2_i]){
+                console.log(sq, arr2[e2_i], sq === arr2[e2_i]);
                 sq_index = e2_i;
                 break;
             }
@@ -18,7 +20,7 @@ function comp(array1, array2){
             result.push(false)
         }
     }
-    if(result.length !== array1.length || result.length !== array2.length){
+    if(result.length !== arr1.length || result.length !== arr2.length){
         return false
     }
     return result.reduce((prev, curr) => prev && curr, true);
