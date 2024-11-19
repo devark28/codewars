84b22109e3688ffc96085964655514754744be03
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-11-19 11:40:07 +0200

    Update Pete, the baker.js

--- 5 kyu/Pete, the baker.js
+++ 5 kyu/Pete, the baker.js
@@ -1,13 +1,8 @@
 function cakes(recipe, available) {
-  console.log(recipe);
-  console.log(available);
-  console.log();
   let possible_cakes = undefined;
   for(let [name, quantity] of Object.entries(recipe)){
-    console.log([name, quantity], " - ", available[name]);
     if(available[name] !== undefined){
       let items = available[name]/quantity
-      console.log(items);
       if(!Number.isNaN(items) && items > 0){
         if(possible_cakes === undefined){
           possible_cakes = Math.trunc(items);
