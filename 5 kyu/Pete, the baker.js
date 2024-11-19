4edc2a5b6bfe9c59e968ed90cc81fa232a723336
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-11-19 12:07:00 +0200

    Update Pete, the baker.js

--- 5 kyu/Pete, the baker.js
+++ 5 kyu/Pete, the baker.js
@@ -1,6 +1,5 @@
 function cakes(recipe, available) {
   return Object.entries(recipe).reduce((possible_cakes, [name, quantity]) => {
-    let items = available[name]/quantity
-    return Math.min(possible_cakes, Math.trunc(items)) || 0;
+    return Math.min(possible_cakes, Math.trunc(available[name]/quantity)) || 0;
   }, Infinity);
 }
