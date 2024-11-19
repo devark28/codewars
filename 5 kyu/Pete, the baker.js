d3c544d51f871a4565b5d6569be52ad6f2325892
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-11-19 12:03:16 +0200

    Update Pete, the baker.js

--- 5 kyu/Pete, the baker.js
+++ 5 kyu/Pete, the baker.js
@@ -1,10 +1,6 @@
 function cakes(recipe, available) {
   return Object.entries(recipe).reduce((possible_cakes, [name, quantity]) => {
     let items = available[name]/quantity
-    if(!Number.isNaN(items)){
-      return Math.min(possible_cakes, Math.trunc(items))
-    }else{
-      return 0;
-    }
+    return !Number.isNaN(items) ? Math.min(possible_cakes, Math.trunc(items)) : 0;
   }, Infinity);
 }
