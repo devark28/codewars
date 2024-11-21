c8a5c8f32d7ee5e5a8fa559ec52eb57b667da4ee
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-11-21 11:33:03 +0200

    Update Sort the odd.js

--- 6 kyu/Sort the odd.js
+++ 6 kyu/Sort the odd.js
@@ -2,9 +2,6 @@
 function sortArray(array) {
     const oddNumbers = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
     return array.map(num => {
-        if (num % 2 !== 0) {
-            return oddNumbers.shift();
-        }
-        return num;
+        return (num % 2 !== 0) ? oddNumbers.shift() : num;
     });
 }
