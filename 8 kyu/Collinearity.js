dd676d1f9a02c17f0fdd0ef0af6c2186e9dfaed6
devark28 <e.shimwa1@alustudent.com>
2024-11-22 14:22:41 +0200

    Updated Collinearity.js

--- 8 kyu/Collinearity.js
+++ 8 kyu/Collinearity.js
@@ -1,7 +1,3 @@
 function collinearity( x1,y1, x2,y2 ) {
-    if ((x1 === 0 && y1 === 0) || (x2 === 0 && y2 === 0)) {
-        return true;
-    }else{
-        return x1 * y2 - y1 * x2 === 0
-    }
+    return ((x1 === 0 && y1 === 0) || (x2 === 0 && y2 === 0)) ? true : x1 * y2 - y1 * x2 === 0;
 }
