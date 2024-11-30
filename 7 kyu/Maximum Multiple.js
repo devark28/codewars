536f02c97a436b1ff0e0072ac6f204c9a2b4ef82
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-11-30 18:13:51 +0200

    Update Maximum Multiple.js

--- 7 kyu/Maximum Multiple.js
+++ 7 kyu/Maximum Multiple.js
@@ -1,6 +1,3 @@
 function maxMultiple(divisor, bound){
-  return Array(bound).fill(null)
-    .map((_, i) => bound - i)
-    .filter(n => n % divisor == 0)
-    .reduce((prev, next) => next > prev ? next : prev, -Infinity)
+  return bound - bound % divisor
 }
