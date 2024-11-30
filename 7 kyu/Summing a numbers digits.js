4866850aa0b67ee78ba439c3e808e14f57976c3d
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-11-30 17:35:11 +0200

    Update Summing a numbers digits.js

--- 7 kyu/Summing a numbers digits.js
+++ 7 kyu/Summing a numbers digits.js
@@ -1,9 +1,7 @@
 function sumDigits(number) {
-  return String(number).split("").slice(
-    number < 0 ? 2 : 1
-  ).reduce((prev, next) => {
+  return Math.abs(number).toString().split("").reduce((prev, next) => {
     return prev + Number(next)
-  }, number < 0 ? +String(number)[1] : +String(number)[0])
+  }, 0)
 }
 
 console.log(10, sumDigits(10));
