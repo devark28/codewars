bc6e145f16a61e90459630ed8ab6634318460e5b
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-12-04 17:13:53 +0200

    Update Human readable duration format.js

--- 4 kyu/Human readable duration format.js
+++ 4 kyu/Human readable duration format.js
@@ -1,28 +1,28 @@
 function formatDuration (secs) {
-  let days = Math.trunc(secs / 86400)
-  let hours = Math.trunc(secs / 3600) - (86400);
-  let minutes = Math.trunc(secs / 60) - (hours * 60);
-  let seconds = secs - (minutes * 60) - (hours * 3600);
-  
-  let result = "";
-  if(hours > 1){
-    result += `${hours} hours`
-  }else if(hours > 0){
-    result += `${hours} hour`
+  if(secs === 0){
+    return 'now'
   }
-  result += (minutes > 0 && hours > 0) ? ", " : ""
-  if(minutes > 1){
-    result += `${minutes} minutes`
-  }else if(minutes > 0){
-    result += `${minutes} minute`
-  }
-  result += (seconds > 0 && minutes > 0) ? " and " : ""
-  if(seconds > 1){
-    result += `${seconds} seconds`
-  }else if(seconds > 0){
-    result += `${seconds} second`
-  }
-  return result ? result : "now";
+  let years = Math.trunc(secs / (60*60*24*365));
+  let days = Math.trunc(secs / (60*60*24)) - (years * 365);
+  let hours = Math.trunc(secs / (60*60))   - (days * 24)    - (years * 365*24);
+  let minutes = Math.trunc(secs / 60)    - (hours * 60)   - (days * 60*24)  - (years * 365*24*60);
+  let seconds = Math.trunc(secs / 1)     - (minutes * 60) - (hours * 60*60) - (days * 60*60*24) - (years * 365*24*60*60);
+//   timestamp_array
+  return [
+    {value: years, label: " year"},
+    {value: days, label: " day"},
+    {value: hours, label: " hour"},
+    {value: minutes, label: " minute"},
+    {value: seconds, label: " second"}
+  ]
+    .filter(comb => comb.value !== 0)
+    .reduce((str, comb) => {
+      return str
+        + (comb.label !== " second" ? ", " : " and ")
+        + comb.value
+        + comb.label
+        + (comb.value > 1 ? "s" : "")
+  }, "").replace(/^(,\s)/, "").replace(/^(\sand\s)/, "").trim()
 }
 
 console.log(formatDuration(1))
@@ -30,3 +30,7 @@ console.log(formatDuration(62))
 console.log(formatDuration(120))
 console.log(formatDuration(3600))
 console.log(formatDuration(3662))
+console.log(formatDuration(15731080)) // '182 days, 1 hour, 44 minutes and 40s ...
+console.log(formatDuration(9660900))
+
+//   return [tsa[0], tsa[1]].join(", ") + ", " + [tsa[2], tsa[3]].join(" and ");
