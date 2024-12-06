e266a573f3a7cafa393ed900bc0f23a4ef0d0931
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-12-06 23:33:56 +0200

    Update Text align justify.js

--- 4 kyu/Text align justify.js
+++ 4 kyu/Text align justify.js
@@ -1,17 +1,19 @@
 function justify(text, width) {
   return text.split(" ").reduce((combo, word, i) => {
     if(combo.lines[combo.current]){
-      if(combo.lines[combo.current].length + word.length + 1 < width){
-        combo.lines[combo.current] += " " + word
+      if(
+        combo.lines[combo.current].reduce((l, w) => l + w.length, 0) +
+        (word.length + 1) < width
+      ){
+        combo.lines[combo.current].push(word)
       }else{
-        combo.current += 1
-        combo.lines[combo.current] = word
+        combo.lines[++combo.current] = [word]
       }
     }else{
-      combo.lines[combo.current] = word
+      combo.lines[combo.current] = [word]
     }
     return combo
-  }, {lines: [], current: 0}).lines.join("\n")
+  }, {lines: [], current: 0})
 }
 console.log(justify('123 45 6', 7))
 console.log("--------")
