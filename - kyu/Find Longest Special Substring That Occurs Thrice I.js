f15c1a299e5e611deb80ab7e9522e4b01522da47
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-12-10 22:59:49 +0200

    Update Find Longest Special Substring That Occurs Thrice I.js

--- - kyu/Find Longest Special Substring That Occurs Thrice I.js
+++ - kyu/Find Longest Special Substring That Occurs Thrice I.js
@@ -1,46 +1,32 @@
 var maximumLength = function (s) {
-    console.log(s)
+    // console.log(s)
     const characters = [...new Set(s)].map(c => ({base: c}))
-    // console.log(characters)
-    const occurences = characters.map((char) => {
+    const characters_details = characters.map((char) => {
         const found = {}
         for (let i = s.length; i > 0; i--) {
             const str = char.base.repeat(i)
             for(let j = 0; j < s.length; j++){
                 if(s.indexOf(str, j) !== -1){
                     found[i] = found[i] ? found[i].concat(s.indexOf(str, j)) : [s.indexOf(str, j)]
-                    // found[i] = found[i] ? ++(found[i]) : 1
                 }
             }
         }
-        // console.log(found)
         const cleaned = Object.fromEntries(Object.entries(found).map(([k,v])=>([k,[...new Set(v)]])))
-        // console.log(cleaned)
         const filtered = Object.entries(cleaned).filter(e => e[1].length >= 3)
-        // const sorted = filtered.sort((a,b)=>a[1].length-b[1].length)
         const sorted = filtered.sort((a,b)=>Number(b[0])-Number(a[0]))
-        // console.log(sorted)
-        // console.log(Object.fromEntries(sorted))
         char.repetitions = Number(sorted?.[0]?.[0]) || -1
         char.occurences = (sorted?.[0]?.[1] || []).length
         char.found_at = sorted?.[0]?.[1] || []
         return char
     })
-    // console.log(occurences)
-    const reduced = occurences.filter(
-            (occu) => (occu.repetitions > 0 && occu.occurences > 0))
-    // console.log(reduced)
-    const biggest = reduced.reduce(
-            (big, occu) => {
-                // console.log(big)
-                // console.log(occu)
-                return Math.max(big.repetitions, occu.repetitions) !== big.repetitions ? occu : big
+    const filtered_characters_details = characters_details.filter(
+            (char) => (char.repetitions > 0 && char.occurences > 0))
+    const highest_repetition_character = filtered_characters_details.reduce(
+            (high, char) => {
+                return Math.max(high.repetitions, char.repetitions) !== high.repetitions ? char : high
             },
-        reduced[0])
-    console.log(biggest)
-    // const biggest = Math.max(...reduced)
-    // console.log(biggest)
-    return biggest?.repetitions || -1
+        filtered_characters_details[0])
+    return highest_repetition_character?.repetitions || -1
 };
 console.log(maximumLength('aaaa'))
 console.log(maximumLength('aaaabb'))
