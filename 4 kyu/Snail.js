fcdcd788a1e16c5ca8cf33ee7fbd0bad3b782295
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-12-13 23:21:45 +0200

    Update Snail.js

--- 4 kyu/Snail.js
+++ 4 kyu/Snail.js
@@ -9,62 +9,43 @@ const array = [
 //     [9, 11, 12, 13],
 //     [14, 15, 16, 17]
 // ]
-const N = array.length
-
 function getCellsAtLevel(level){
     let results = Array((4*(array.length-(2*level)))-4)
-    // console.log(results)
-    for(let i=level; i<(N-level); i++){
+    for(let i=level; i<(array.length-level); i++){
         let N = (array.length-(2*level))
         x = level
         y = i
         
-        // console.log([x, y])
         let p1 = i - level
         let p2 = ((4*N)-4) - (i - level)
-        // console.log()
-        // console.log([p1, p2])
         if(x !== y){
-            // console.log(array[x][y])
-            // console.log(array[y][x])
-            
             results[p1] = array[x][y]
             results[p2] = array[y][x]
         }else{
-            // console.log(array[y][x])
-            
             results[p1] = array[x][y]
         }
     }
-    for(let i=1+level; i<(N-level); i++){
+    for(let i=1+level; i<(array.length-level); i++){
         let N = (array.length-(2*level))
         x = i
         y = N-(1-level)
-          
-        // console.log([x, y])
+        
         let p1 = N + (i - (1+level))
         let p2 = ((4*N)-4) - (N + (i - (1+level)))
-        // console.log()
-        // console.log([p1, p2])
         if(x !== y){
-            // console.log(array[x][y])
-            // console.log(array[y][x])
-            
             results[p1] = array[x][y]
             results[p2] = array[y][x]
         }else{
-            // console.log(array[y][x])
-            
             results[p1] = array[x][y]
         }
     }
     return results
 }
 
-console.log(getCellsAtLevel(0).concat(getCellsAtLevel(1)))
-// console.log(getCellsAtLevel(0))
-// console.log(getCellsAtLevel(1))
-// getCellsAtLevel(1)
-
+let results = []
+for(let i=0; i<(Math.ceil(array.length/2));i++){
+  results = results.concat(getCellsAtLevel(i))
+}
+console.log(results)
 
 
