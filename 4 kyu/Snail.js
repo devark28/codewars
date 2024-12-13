4ba36f528b45414e4ee2aeece83dbb2d117b9450
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2024-12-13 23:46:26 +0200

    Update Snail.js

--- 4 kyu/Snail.js
+++ 4 kyu/Snail.js
@@ -8,13 +8,13 @@ snail = function(array) {
 
 function getCellsAtLevel(array, level){
     let results = Array((4*(array.length-(2*level)))-4)
+    let N = (array.length-(2*level))
     for(let i=level; i<(array.length-level); i++){
-        let N = (array.length-(2*level))
-        x = level
-        y = i
-        
+        let x = level
+        let y = i
         let p1 = i - level
-        let p2 = ((4*N)-4) - (i - level)
+        let p2 = ((4*N)-4) - p1
+        
         if(x !== y){
             results[p1] = array[x][y]
             results[p2] = array[y][x]
@@ -23,12 +23,11 @@ function getCellsAtLevel(array, level){
         }
     }
     for(let i=1+level; i<(array.length-level); i++){
-        let N = (array.length-(2*level))
-        x = i
-        y = N-(1-level)
-        
+        let x = i
+        let y = N-(1-level)
         let p1 = N + (i - (1+level))
-        let p2 = ((4*N)-4) - (N + (i - (1+level)))
+        let p2 = ((4*N)-4) - p1
+        
         if(x !== y){
             results[p1] = array[x][y]
             results[p2] = array[y][x]
