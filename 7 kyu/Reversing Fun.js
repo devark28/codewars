7c046285b1aaa38f18db5c0f2245f92fd21650ce
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-01-10 13:35:35 +0200

    Update Reversing Fun.js

--- 7 kyu/Reversing Fun.js
+++ 7 kyu/Reversing Fun.js
@@ -1,14 +1,5 @@
 function flipNumber(n)
 {
-//   let input = n.split("");
-//   return n.split("").reduce((prev) => {
-// //     input.reverse();
-// //     prev += input.shift();
-// //     prev += input.reverse().shift();
-// //     prev = prev + input.reverse().shift();
-//     return prev + input.reverse().shift();;
-//   }, "");
-  
   return ((input) => {
     return [...n].reduce((prev) => {
       return prev + input.reverse().shift();;
