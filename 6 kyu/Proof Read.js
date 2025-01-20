159d6f86d4bc7df57ba7699109e24f2fd27a4469
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-01-20 14:44:08 +0200

    Update Proof Read.js

--- 6 kyu/Proof Read.js
+++ 6 kyu/Proof Read.js
@@ -1,7 +1,4 @@
 function proofread (str) { 
   return str.toLowerCase().replaceAll(/ie/g, 'ei').replaceAll(/^\w|\.\s(\w)/g, (s) => s.toUpperCase());
 }
-
-
-// console.log(proofread("ShE deCIeved HiM.")); // "She deceived him."
-console.log(proofread("SHe rEcieveD a pIegNor. ShE deCIeved HiM.")); // "She received a peignor. She deceived him."
+console.log(proofread("ShE deCIeved HiM.")); // "She deceived him."
