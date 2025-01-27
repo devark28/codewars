ce62330d92455cfce5f9fb3e9c465d04b85eda54
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-01-27 12:10:06 +0200

    Create Minimize Sum Of Array.js

---
+++ 7 kyu/Minimize Sum Of Array.js
@@ -0,0 +1,13 @@
+function minSum(arr) {
+  arr.sort((a,b)=>b-a);
+  return Array(arr.length/2).fill(null).map(() => arr.shift()*arr.pop()).reduce((acc,num)=>acc+num,0);
+}
+
+// using parameter scope to sort
+// function minSum(arr, sorted=arr.sort((a,b)=>b-a)) {
+//   return Array(sorted.length/2).fill(null).map(() => sorted.shift()*sorted.pop()).reduce((acc,num)=>acc+num,0);
+// }
+
+console.log(minSum([5,4,2,3])); // [ 5, 4, 3, 2 ]
+console.log(minSum([12,6,10,26,3,24])); // [ 26, 24, 12, 10, 6, 3 ]
+console.log(minSum([9,2,8,7,5,4,0,6])); // [ 9, 8, 7, 6, 5, 4, 2, 0 ]
