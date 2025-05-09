8d4b8e64ca886aa2ec6e070697a6ae810d07be82
Bruce Emmanuel SHIMWA <82113556+devark28@users.noreply.github.com>
2025-05-09 21:31:34 +0200

    Create Screen Locking Patterns.js

---
+++ 3 kyu/Screen Locking Patterns.js
@@ -0,0 +1,80 @@
+const nodes = {
+  'A': {
+    inAccessibles: ['C', 'I', 'G'],
+    blocking: [],
+  },
+  'B': {
+    inAccessibles: ['H'],
+    blocking: ['AC', 'CA'],
+  },
+  'C': {
+    inAccessibles: ['A', 'G', 'I'],
+    blocking: [],
+  },
+  'D': {
+    inAccessibles: ['F'],
+    blocking: ['AG', 'GA'],
+  },
+  'E': {
+    inAccessibles: [],
+    blocking: ['AI', 'BH', 'CG', 'DF', 'FD', 'GC', 'HB', 'IA'],
+  },
+  'F': {
+    inAccessibles: ['D'],
+    blocking: ['CI', 'IC'],
+  },
+  'G': {
+    inAccessibles: ['A', 'C', 'I'],
+    blocking: [],
+  },
+  'H': {
+    inAccessibles: ['B'],
+    blocking: ['GI', 'IG'],
+  },
+  'I': {
+    inAccessibles: ['G', 'A', 'C'],
+    blocking: [],
+  },
+};
+
+function countPatternsFrom(firstPoint, length) {
+  if (length < 1 || length > 9) return 0;
+  if (length === 1) return 1;
+
+  let window = [];
+  for(let i = 0; i < length-1; i++){
+    if(!window.length) {
+      window = [makeWindowNode(firstPoint)];
+    } else{
+      window = window.reduce((acc, wn) => {
+        return acc.concat(
+          wn.accessibles.map(
+            n => makeWindowNode(n, wn.visited.concat(wn.node))
+          )
+        );
+      }, []);
+    }
+  }
+  // console.log(firstPoint, length, "->", window);
+  return window.reduce((acc, wn) => acc + wn.accessibles.length, 0);
+}
+
+function makeWindowNode(node, visited = []) {
+  return {
+    node: node,
+    accessibles: getAccessibles(node, visited),
+    visited: visited
+  };
+}
+
+function getAccessibles(node, visited) {
+  return Object.keys(nodes).filter((n) => {
+    const inAccessibles = nodes[node].inAccessibles.filter(
+      (nn) => !visited.reduce(
+          (acc, v) => acc || nodes[v].blocking.includes(node+n),
+          false
+        )
+      ).concat(node).concat(visited);
+    return !inAccessibles.includes(n);
+  });
+}
